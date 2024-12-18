f4af97d3cf23feb642a0de8e4f3256cf26552ff9
varshneypragyan <pragyanvarshney@gmail.com>
2024-12-18 22:25:50 +0530

    adding unit test

---
+++ app/app/calc.py
@@ -0,0 +1,11 @@
+"""
+Calculation functions
+"""
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+def subtract(x, y):
+    """Subtract x and y and return result."""
+    return x - y
