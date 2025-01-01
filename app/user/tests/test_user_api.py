0d2a052ea9f835d48f97683b2f06d889e30dd333
Pragyan Varshney <pragyan.varshney@clickpost.ai>
2025-01-01 17:56:16 +0530

    adding user APIs

--- app/user/tests/test_user_api.py
+++ app/user/tests/test_user_api.py
@@ -11,7 +11,7 @@ from rest_framework import status
 
 CREATE_USER_URL = reverse('user:create')
 TOKEN_URL = reverse('user:token')
-
+ME_URL = reverse('user:me')
 
 def create_user(**params):
     """Create and return a new user."""
